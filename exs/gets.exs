eeb918123508cd3e88aeaece61ce75e9249087c7
Samuel Ventura <samuel.ventura@yeico.com>
2022-10-04 01:58:13 -0500

    Added gets.exs

---
+++ exs/gets.exs
@@ -0,0 +1,3 @@
+# mix run exs/gets.exs
+# four arrow keys show as ^[[C^[[D^[[B^[[A
+IO.gets(">")
--- exs/iex.exs
+++ exs/iex.exs
@@ -1,13 +1,4 @@
 # mix run exs/iex.exs
 # four arrow keys show as ^[[C^[[D^[[B^[[A
-# in both IO.gets and IEx.start
-
-IO.inspect(:init.get_arguments())
-IO.inspect(Application.get_env(:elixir, :ansi_enabled))
-IO.gets(">")
+System.no_halt(true)
 IEx.start()
-
-# wait without messing with stdio
-receive do
-  any -> IO.inspect(any)
-end
