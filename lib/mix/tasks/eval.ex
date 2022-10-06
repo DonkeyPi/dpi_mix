217adc16a08f67236f798c92d61d397f45314dc9
Samuel Ventura <samuel.ventura@yeico.com>
2022-10-06 11:38:03 -0500

    Eval file and eval output capture.

--- lib/mix/tasks/eval.ex
+++ lib/mix/tasks/eval.ex
@@ -6,10 +6,19 @@ defmodule Mix.Tasks.Ash.Eval do
 
   def run(args) do
     :ssh.start()
-    code = Enum.join(args, " ")
+    path_code = Enum.join(args, " ")
     ash = Ash.load_config()
+
+    {type, value, code} =
+      if String.starts_with?(path_code, "file:") do
+        path_code = String.replace_leading(path_code, "file:", "")
+        {"file", path_code, File.read!(path_code)}
+      else
+        {"code", path_code, path_code}
+      end
+
     Mix.shell().info("Evaluating on: #{ash.name}@#{ash.runtime}")
-    Mix.shell().info("Evaluating code: #{code}")
+    Mix.shell().info("Evaluating #{type}: #{value}")
     host = ash.host |> String.to_charlist()
     user = ash.name |> Atom.to_charlist()
     opts = [silently_accept_hosts: true, user: user]
