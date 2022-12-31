5ebcb0499637acc80f5c29cfc496212d5748bcb5
Samuel Ventura <samuel.ventura@yeico.com>
2022-12-31 17:18:41 -0600

    Mix ash.run path_to_script.exs.

--- lib/mix/tasks/run.ex
+++ lib/mix/tasks/run.ex
@@ -2,9 +2,32 @@ defmodule Mix.Tasks.Ash.Run do
   use Mix.Task
   alias Mix.Tasks.Ash
 
-  @shortdoc "Runs application on selected runtime"
+  @shortdoc "Runs script or application on selected runtime"
 
-  def run(_args) do
+  def run(args) do
+    case args do
+      [] -> run_app()
+      _ -> run_script(Enum.join(args, " "))
+    end
+  end
+
+  defp run_script(path) do
+    :ssh.start()
+    ash = Ash.basic_config(true)
+    code = File.read!(path)
+    Mix.shell().info("Running on: #{Ash.runtime_id(ash)}")
+    Mix.shell().info("Running script: #{path}")
+    host = ash.host |> String.to_charlist()
+    user = ash.name |> Atom.to_charlist()
+    opts = [silently_accept_hosts: true, user: user]
+    {:ok, conn} = :ssh.connect(host, ash.port, opts)
+    {:ok, chan} = :ssh_connection.session_channel(conn, Ash.toms())
+    :success = :ssh_connection.subsystem(conn, chan, 'runtime', Ash.toms())
+    :ok = :ssh_connection.send(conn, chan, "eval " <> code, Ash.toms())
+    Ash.stdout(conn, chan)
+  end
+
+  defp run_app() do
     Mix.Task.run("ash.upload", ["keep"])
     ash = Ash.init()
     Mix.shell().info("Running on: #{Ash.runtime_id(ash)}")
--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Ash.Mix.MixProject do
   def project do
     [
       app: :ash_mix,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       archives: [nerves_bootstrap: "~> 1.11"],
