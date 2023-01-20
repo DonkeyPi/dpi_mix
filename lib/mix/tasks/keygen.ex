720088e9afe72c086d7fbd671b721d8b2e282554
Samuel Ventura <samuel.ventura@yeico.com>
2023-01-19 23:02:23 -0600

    Keygen filename option and pubkey comment.

--- lib/mix/tasks/keygen.ex
+++ lib/mix/tasks/keygen.ex
@@ -3,20 +3,31 @@ defmodule Mix.Tasks.Dpi.Keygen do
 
   @shortdoc "Generate RSA key in PEM format"
 
-  def run(_args) do
-    key_pair()
+  def run(args) do
+    case args do
+      [] -> key_pair()
+      [name] -> key_pair(name)
+      _ -> Mix.raise("use: mix dpi.keygen <filename>")
+    end
   end
 
-  def key_pair(bits \\ 4096) do
+  def key_pair(name \\ nil) do
     {:RSAPrivateKey, _, modulus, publicExponent, _, _, _, _exponent1, _, _, _otherPrimeInfos} =
-      rsa_private_key = :public_key.generate_key({:rsa, bits, 65537})
+      rsa_private_key = :public_key.generate_key({:rsa, 4096, 65537})
 
     pem_entry = :public_key.pem_entry_encode(:RSAPrivateKey, rsa_private_key)
     private_key = :public_key.pem_encode([pem_entry])
 
+    {:ok, hostname} = :inet.gethostname()
+    comment = "#{System.get_env("USER")}@#{hostname}"
     rsa_public_key = {:RSAPublicKey, modulus, publicExponent}
-    public_key = :ssh_file.encode([{rsa_public_key, []}], :openssh_key)
+    public_key = :ssh_file.encode([{rsa_public_key, [{:comment, comment}]}], :openssh_key)
 
-    IO.puts(~s/PUBLIC KEY:\n\n#{public_key}\n\nPRIVATE KEY\n\n#{private_key}/)
+    if name == nil do
+      IO.puts(~s/PUBLIC KEY:\n\n#{public_key}\n\nPRIVATE KEY\n\n#{private_key}/)
+    else
+      File.write!("#{name}.pem", private_key)
+      File.write!("#{name}.pub", public_key)
+    end
   end
 end
