77292194f857a65e73e56fb61d00393871310667
Samuel Ventura <samuel.ventura@yeico.com>
2023-01-31 16:19:32 -0600

    Start, stop, and uninstall accept app name as cli arg.

--- lib/mix/tasks/stop.ex
+++ lib/mix/tasks/stop.ex
@@ -4,12 +4,20 @@ defmodule Mix.Tasks.Dpi.Stop do
 
   @shortdoc "Stops application on selected runtime"
 
-  def run(_args) do
+  def run(args) do
+    {with_app, app_name} =
+      case {args, File.exists?("mix.exs")} do
+        {[app_name], _} -> {false, app_name}
+        {[], true} -> {true, nil}
+        {[], _} -> Mix.raise("Not in app folder")
+      end
+
     :ssh.start()
-    dpi = Dpi.init()
+    dpi = Dpi.basic_config(with_app)
+    app_name = if with_app, do: dpi.name, else: app_name
     Mix.shell().info("Stopping on: #{Dpi.runtime_id(dpi)}")
     host = dpi.host |> String.to_charlist()
-    user = dpi.name |> Atom.to_charlist()
+    user = "#{app_name}" |> String.to_charlist()
     opts = [silently_accept_hosts: true, user: user] |> Dpi.add_user_dir()
     {:ok, conn} = :ssh.connect(host, dpi.port, opts)
     {:ok, chan} = :ssh_connection.session_channel(conn, Dpi.toms())
